027cf6d4e9006059ab8787c14f6088c547724f01
lmannelli <lmannelli@itba.edu.ar>
2024-02-17 11:57:03 -0300

    BraTS space corrected

--- main.py
+++ main.py
@@ -25,8 +25,11 @@ def save_nifti_files(global_path, output_path):
 
 if __name__ == "__main__":
     # LOAD PATH FILES
-    global_path = '/Users/Maxy/Desktop/GBM/Herramienta CAD/TCGA-GBM/TCGA-02-0003/06-08-1997-MRI BRAIN WWO CONTRAMR-81239/*' 
-    output_path = '/Users/Maxy/Desktop/GBM/Herramienta CAD/TCGA-GBM/TCGA-02-0003/NIFTI_FILES'
+    #MRI example dataset
+    global_path = '/Users/Maxy/Desktop/GBM/Herramienta_CAD/TCGA-GBM/TCGA-02-0003/06-08-1997-MRI BRAIN WWO CONTRAMR-81239/*' 
+    output_path = '/Users/Maxy/Desktop/GBM/Herramienta_CAD/TCGA-GBM/TCGA-02-0003/NIFTI_FILES'
+    #atlas dataset
+    atlas_path_t1 = '/Users/Maxy/Desktop/GBM/Herramienta_CAD/sri24_spm8/templates'
 
     save_nifti_files(global_path, output_path)
 
@@ -34,22 +37,45 @@ if __name__ == "__main__":
     t1c = Preprocessing(output_path, 't1c_test.nii')
     t2 = Preprocessing(output_path, 't2_test.nii')
     flair = Preprocessing(output_path, 'flair_test.nii')
+    atlas_t1 = Preprocessing(atlas_path_t1,'T1.nii' )
 
     # CO REGISTRATION
     template = t1.temp
+    brats_flag = False
+    t1c.coregistration(template,'Similarity', brats_flag)
+    t2.coregistration(template,'Similarity', brats_flag)
+    flair.coregistration(template,'Similarity', brats_flag)
 
-    t1c.registration(template)
-    t2.registration(template)
-    flair.registration(template)
+    # Native space transformation 
+    atlas = atlas_t1.temp
+    brats_flag = True
+    matrix = t1.coregistration(atlas,'SyN', brats_flag)
+
+    #Apply T1-Atlas transformation matrix to t1c, t2, flair
+    t1c.apply_transformation(atlas,matrix)
+    t2.apply_transformation(atlas,matrix)
+    flair.apply_transformation(atlas,matrix)
 
     # T1 Brain extraction 
-    prob_brain_mask = brain_extraction(template, modality="t1", verbose=True,)
+    prob_brain_mask = brain_extraction(t1.reg, modality="t1", verbose=True,)
 
     # GET T1 MASK
     brain_mask_t1 = ants.get_mask(prob_brain_mask, low_thresh=0.5)
-    masked_t1 = ants.mask_image(template, brain_mask_t1)
+    masked_t1 = ants.mask_image(t1.reg, brain_mask_t1)
 
     # Now we have the t1 mask, we do the same for t1c, t2 and flair
     t1c.mask_image(brain_mask_t1)
     t2.mask_image(brain_mask_t1)
     flair.mask_image(brain_mask_t1)
+
+    ants.plot(masked_t1, figsize=1, axis=2)
+    print('masked t1:', masked_t1)
+
+    ants.plot(t1c.masked, figsize=1, axis=2)
+    print('masked t1:', t1c.masked)
+
+    ants.plot(t2.masked, figsize=1, axis=2)
+    print('masked t1:', t2.masked)
+
+    ants.plot(flair.masked, figsize=1, axis=2)
+    print('masked t1:', flair.masked)
--- utils.py
+++ utils.py
@@ -6,16 +6,30 @@ class Preprocessing:
     def __init__(self, output_path, document):
         self.path = os.path.join(output_path, document)
         self.temp = ants.image_read(self.path, reorient='IAL')
-        
-    def registration(self, template):
+
+    def coregistration(self, template, transform, brats_flag):
         self.transformation = ants.registration(
                 fixed=template,
                 moving=self.temp, 
-                type_of_transform='SyN',
+                type_of_transform=transform,
                 verbose=True
             )
         self.reg = self.transformation['warpedmovout']
+        if(brats_flag == True):
+            return self.transformation
 
+    
+    def apply_transformation(self,atlas,matrix):
+        transform_paths = [matrix['fwdtransforms'][0], matrix['fwdtransforms'][1]]
+        self.transformed_image = ants.apply_transforms(
+                fixed=atlas,  # must be atlas
+                moving=self.reg,
+                transformlist=transform_paths,
+                interpolator='linear',  # Using nearestNeighbour to preserve intensities
+                imagetype=0,  
+                verbose=True
+            )
+        self.res = self.transformed_image
     def mask_image(self, brain_mask):
-        self.masked = ants.mask_image(self.reg, brain_mask)
+        self.masked = ants.mask_image(self.res, brain_mask)
 
