52745e66b0e03622edf19ad15f8d134c468e1b60
tsarthou <tsarthou@itba.edu.ar>
2024-03-20 18:26:31 -0300

    limpieza del main

--- main.py
+++ main.py
@@ -11,58 +11,21 @@ from antspynet.utilities import brain_extraction
 from scipy.ndimage import shift
 from scipy.signal import correlate
 
-def save_nifti_files(global_path, output_path):
-    for i, modality in enumerate(glob(global_path)): #we save the nifti files 
-        document = ''
-        if i == 0 : 
-            document = 't1c_test.nii'
-        elif i == 1 : 
-            document = 'flair_test.nii'
-        elif i == 2 :
-            document = 't2_test'
-        else :
-            document = 't1_test'
-        
-        dicom2nifti.dicom_series_to_nifti(modality, os.path.join(output_path, document))
-
-def dice_coefficient(volume1, volume2):
-    intersection = np.sum(volume1 * volume2)
-    total_voxels = np.sum(volume1) + np.sum(volume2)
-
-    dice = (2.0 * intersection) / total_voxels
-
-    return dice
-
-def max_correlation(control, imagen):
-
-    # Calculate 3D cross-correlation 
-    correlation = correlate(control, imagen, mode="same", method="fft")
-
-    # Max correlation coords
-    coords = np.unravel_index(np.argmax(correlation), correlation.shape)
-
-    # Calculate shift
-    shift_values = np.array(coords) - np.array(control.shape) // 2
-
-    # Apply shift
-    im_shifted = shift(imagen, shift_values, mode='constant', cval=0)
-
-    return im_shifted
 
 if __name__ == "__main__":
     # LOAD PATH FILES
-    #MRI example dataset
-    global_path = "C:\\Users\\Pablo\\Desktop\\PF\\GUI\\Imagenes\\TCGA-GBM\\TCGA-06-5413\\06-17-2008-18002\\*" 
-    output_path = "C:\\Users\\Pablo\\Desktop\\PF\\GUI\\Imagenes\\TCGA-GBM\\NIFTI_FILES"
+    #MRI example dataset 
+    global_path = "C:\\Users\\sarth\\OneDrive\\Escritorio\\ITBA\\Proyecto Final\\CAD\\GBM_upenn\\upenn_crudas\\UPENN-GBM-00031\\*" 
+    output_path = "C:\\Users\\sarth\\OneDrive\\Escritorio\\ITBA\\Proyecto Final\\CAD\\NIFTI_ouputs"
     #atlas dataset
-    atlas_path_t1 = "C:\\Users\\Pablo\\Desktop\\PF\\GUI\\Imagenes\\sri24_spm8\\templates"
+    atlas_path_t1 = "C:\\Users\\sarth\\OneDrive\\Escritorio\\ITBA\\Proyecto Final\\CAD\\sri24_spm8\\templates"
 
     save_nifti_files(global_path, output_path)
 
-    t1 = Preprocessing(output_path, 't1_test.nii')
     t1c = Preprocessing(output_path, 't1c_test.nii')
-    t2 = Preprocessing(output_path, 't2_test.nii')
+    t1 = Preprocessing(output_path, 't1_test.nii')
     flair = Preprocessing(output_path, 'flair_test.nii')
+    t2 = Preprocessing(output_path, 't2_test.nii')
     atlas_t1 = Preprocessing(atlas_path_t1,'T1.nii' )
 
     # CO REGISTRATION
@@ -96,23 +59,23 @@ if __name__ == "__main__":
     t2.mask_image(brain_mask_t1)
     flair.mask_image(brain_mask_t1)
 
-    # Plot
+    # # Plot
 
-    ants.plot(masked_t1, figsize=1, axis=2)
-    print('masked t1:', masked_t1)
+    # ants.plot(masked_t1, figsize=1, axis=2)
+    # print('masked t1:', masked_t1)
 
-    ants.plot(t1c.masked, figsize=1, axis=2)
-    print('masked t1c:', t1c.masked)
+    # ants.plot(t1c.masked, figsize=1, axis=2)
+    # print('masked t1c:', t1c.masked)
 
-    ants.plot(t2.masked, figsize=1, axis=2)
-    print('masked t2:', t2.masked)
+    # ants.plot(t2.masked, figsize=1, axis=2)
+    # print('masked t2:', t2.masked)
 
-    ants.plot(flair.masked, figsize=1, axis=2)
-    print('masked flair:', flair.masked)
+    # ants.plot(flair.masked, figsize=1, axis=2)
+    # print('masked flair:', flair.masked)
 
     # Import control image
 
-    path_control = "C:\\Users\\Pablo\\Desktop\\PF\\GUI\\Imagenes\\Pre-operative_TCGA_GBM_NIfTI_and_Segmentations\\TCGA-06-5413\\TCGA-06-5413_2008.06.17_t1.nii.gz"
+    path_control = "C:\\Users\\sarth\\OneDrive\\Escritorio\\ITBA\\Proyecto Final\\CAD\\GBM_upenn\\upenn_preprocessed\\UPENN-GBM-00031_11\\UPENN-GBM-00031_11_T1.nii"
     control = ants.image_read(path_control, reorient='IAL')
     
     # Otsu binarization
@@ -126,15 +89,15 @@ if __name__ == "__main__":
     #print("Propia:", bm1.numpy().shape)
     print("Control:", bcontrol.numpy().shape)
 
-    fig, axs = plt.subplots(1, 2, figsize=(10, 10))
-    axs[0].imshow(bm1_shifted[:][:][20], cmap='gray')
-    #axs[0].imshow(bm1.numpy()[:][:][20], cmap='gray')
-    axs[0].axis('off')
-    axs[0].set_title('T1 binarizada')
-    axs[1].imshow(bcontrol.numpy()[:][:][20], cmap='gray')
-    axs[1].axis('off')
-    axs[1].set_title('Control binarizada')
-    plt.show()
+    # fig, axs = plt.subplots(1, 2, figsize=(10, 10))
+    # axs[0].imshow(bm1_shifted[:][:][20], cmap='gray')
+    # #axs[0].imshow(bm1.numpy()[:][:][20], cmap='gray')
+    # axs[0].axis('off')
+    # axs[0].set_title('T1 binarizada')
+    # axs[1].imshow(bcontrol.numpy()[:][:][20], cmap='gray')
+    # axs[1].axis('off')
+    # axs[1].set_title('Control binarizada')
+    # plt.show()
 
     # Dice
     print(glob(global_path))
--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
 import os
 import ants
+import dicom2nifti
+from glob import glob
+import numpy as np
+from scipy.ndimage import shift
+from scipy.signal import correlate
 
 
 class Preprocessing:
@@ -34,3 +39,40 @@ class Preprocessing:
         self.masked = ants.mask_image(self.res, brain_mask)
 
 
+def save_nifti_files(global_path, output_path):
+    for i, modality in enumerate(glob(global_path)): #we save the nifti files 
+        document = ''
+        if i == 0 : 
+            document = 't2_test'
+        elif i == 1 : 
+            document = 't1_test.nii'
+        elif i == 2 :
+            document = 'flair_test.nii'
+        else :
+            document = 't1c_test'
+        
+        dicom2nifti.dicom_series_to_nifti(modality, os.path.join(output_path, document))
+
+def dice_coefficient(volume1, volume2):
+    intersection = np.sum(volume1 * volume2)
+    total_voxels = np.sum(volume1) + np.sum(volume2)
+
+    dice = (2.0 * intersection) / total_voxels
+
+    return dice
+
+def max_correlation(control, imagen):
+
+    # Calculate 3D cross-correlation 
+    correlation = correlate(control, imagen, mode="same", method="fft")
+
+    # Max correlation coords
+    coords = np.unravel_index(np.argmax(correlation), correlation.shape)
+
+    # Calculate shift
+    shift_values = np.array(coords) - np.array(control.shape) // 2
+
+    # Apply shift
+    im_shifted = shift(imagen, shift_values, mode='constant', cval=0)
+
+    return im_shifted
